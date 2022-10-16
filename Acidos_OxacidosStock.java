a77c072e74826509293f965571814f02d9f7935a
Jean Franco Boom Bolaño <55298889+TheFive40@users.noreply.github.com>
2022-10-16 18:30:55 -0500

    Update Acidos_OxacidosStock.java

--- Acidos_OxacidosStock.java
+++ Acidos_OxacidosStock.java
@@ -1,18 +1,22 @@
-package MetodoStock;
+package MetodoStock; //Nombre del paquete donde esta ubicada nuestra clase
 
-import java.util.Scanner;
+import java.util.Scanner; //Importamos la clase Scanner de las librerias de Java util
 
-public class Acidos_OxacidosStock {
-	public void Acidos_Oxacidoss() {
-	Scanner entrada = new Scanner(System.in);
-	System.out.println("Ingrese un compuesto por favor");
-	String compuesto = entrada.nextLine();
+public class Acidos_OxacidosStock { // Nombre de la Clase 
+	public void Acidos_Oxacidoss() { //Metodo Setter 
+	Scanner entrada = new Scanner(System.in); //Creamos el Objeto de Entrada (INPUT)
+	System.out.println("Ingrese un compuesto por favor"); //Mostramos un mensaje por consola pidiendole al usuarios que nos ingrese un compuesto 
+	String compuesto = entrada.nextLine(); //Guardamos el compuesto ingresado por el usuario en la variable Compuesto
+		//Evaluamos con condicionales  lo que esta guardado en la variable compuesto es "H2SO4" En caso de ser verdadera esta condicion entonces estamos 
+		//Hablando de un SULFATO (VI) DE HIDROGENO  NOS IMPRIME EL RESULTADO CON SU NUMERO DE OXIDACION EN NUMEROS ROMANOS
+		//EN CASO QUE ESTA CONDICION HAYA SIDO FALSA PASARIA A EVALUAR LA SIGUIENTE CONDICION Y EN CASO QUE ESA TAMBIEN FUERA FALSA IRIA CON LAS DEMAS 
+		//ASI SUCESIVAMENTE 
 	if (compuesto.contentEquals("H2SO4")) {
 		System.out.println("Es un SULFATO (VI) DE HIDROGENO");
 	}else if (compuesto.contentEquals("HClO")) {
-		System.out.println("Es un Clorato (I) de Hidrógeno");
+		System.out.println("Es un Clorato (I) de HidrÃ³geno");
 	}else if (compuesto.contentEquals("HNO3")){
-		System.out.println("Es un Nitrato (V) de hidrógeno");
+		System.out.println("Es un Nitrato (V) de hidrÃ³geno");
 	}else if(compuesto.contentEquals("H2CO2")) {
 		System.out.println("Es un Carbono  (II) de Hidrogeno");
 	}else if (compuesto.contentEquals("H3PO4")) {
