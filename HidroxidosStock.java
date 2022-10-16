7b53194bbdf58268e00d547b2f65f9a61466bfa7
Jean Franco Boom Bolaño <55298889+TheFive40@users.noreply.github.com>
2022-10-16 18:36:50 -0500

    Update HidroxidosStock.java

--- HidroxidosStock.java
+++ HidroxidosStock.java
@@ -1,13 +1,19 @@
-package MetodoStock;
+package MetodoStock; //Nombre de nuestro paquete
 
-import java.util.Scanner;
+import java.util.Scanner; //Importamos las librerias pertinentes 
 
-public class HidroxidosStock {
-public void Hidroxidos() {
-	Scanner entrada = new Scanner(System.in);
-	System.out.println("Ingrese un compuesto por favor");
-	String compuesto = entrada.nextLine();
+public class HidroxidosStock { //Nombre de la clase 
+public void Hidroxidos() { //Metodo Setter 
+	Scanner entrada = new Scanner(System.in); //HACEMOS UNA ENTRADA DE DATOS INSTANCIANDO LA CLASE SCANNER 
+	System.out.println("Ingrese un compuesto por favor"); //POR MENSAJE LE INDICAMOS AL USUARIO QUE NOS INGRESE UN COMPUESTO
+	String compuesto = entrada.nextLine(); //EL USUARIO INGRESA EL COMPUESTO Y LO GUARDA EN LA VARIABLE COMPUESTO
+	//CON UN IF PROCEDEMOS A EVALUAR SI LO QUE GUARDA LA VARIABLE COMPUESTO ES UN "OH" RECORDEMOS QUE PARA LOS HIDROXIDOS  DEBEN ESTAR ACOMPAÑADOS DEL OH
+	//POR LO TANTO SE DEBE EVALUAR ESTA CONDICION SI LA CONDICION ES VERDADERA VOLVERA A EVALUAR UNA CONDICION DENTRO DE OTRA CONDICION ESTO LO LLAMAMOS IF ANIDADOS
+	//ES DECIR QUE PARA QUE SE LEAN LOS OTROS CONDICIONALES ANTES DEBE EVALUAR SI EL COMPUESTO INGRESADO ES UN HIDROXIDO
 	if(compuesto.contains("OH")) {
+		//UNA VEZ EVALUADO Y DE SER VERDADERA LA CONDICION PASARA A EVALUAR SI EL COMPUESTO TIENE "CuOH" EN CASO DE SER VERDADERA ESTA CONDICION 
+		//ENTONCES ESTAREMOS HABLANDO DE UN HIDROXIDO DE COBRE 
+		//ASI LO HARA CON EL RESTO DE CONDICIONES EN CASO DE QUE ESTA NO SEA VERDADERA PASARA A EVALUAR LAS DEMAS 
 		if(compuesto.contains("CuOH")){
 			System.out.println("Es un  HIDROXIDO de COBRE (I)");
 		}else if (compuesto.contentEquals("Cu(OH)2")) {
