a9ba3f6117e8d532456cc2b2f186868ac4f1c6f3
Jean Franco Boom Bolaño <55298889+TheFive40@users.noreply.github.com>
2022-10-16 19:04:21 -0500

    Update Sales_Haloideas_Stock.java

--- Sales_Haloideas_Stock.java
+++ Sales_Haloideas_Stock.java
@@ -1,14 +1,17 @@
-package MetodoStock;
+package MetodoStock; //PAQUETE
 
-import java.util.Scanner;
+import java.util.Scanner; //IMPORTAMOS LAS LIBRERIAS DE JAVA UTIL DE LA CLASE SCANNER 
 
-public class Sales_Haloideas_Stock {
-	public void Haloideas () {
-	Scanner Entrada = new Scanner(System.in);
-	System.out.println("Ingrese un compuesto");
-	String compuesto = Entrada.nextLine();
+public class Sales_Haloideas_Stock { //CLASE 
+	public void Haloideas () { //METODO
+	Scanner Entrada = new Scanner(System.in); //INDICAMOS QUE QUEREMOS QUE EL USUARIO  NOS INGRESE UN DATO DESDE EL TECLADO DE LA CONSOLA
+	System.out.println("Ingrese un compuesto"); //LE DECIMOS QUE NOS INGRESE UN COMPUESTO ATRAVEZ DE UN MENSAJE QUE APARECERA EN LA CONSOLA
+	String compuesto = Entrada.nextLine(); //GUARDAMOS LO QUE NOS INGRESO EL USUARIO EN LA VARIABLE COMPUESTO
+		//EVALUAMOS CON UN CONDICIONAL SI EL COMPUESTO INGRESADO POR EL USUARIO ES UN "FeCl2"
 	if(compuesto.contains("FeCl2")) {
-		System.out.println("Cloruro de Hierro (II)");
+		//SI ES VERDADERA EJECUTA ESTA LINEA DE CODIGO
+		System.out.println("Cloruro de Hierro (II)"); //MUESTRA UN MENSAJE POR CONSOLA QUE LE INDIQUE LA NOMENCLATURA 
+		//ES UN CLORURO DE HIERRO CON SU ESTADO DE OXIDACION (III)
 		
 	}else if (compuesto.contains("PbCl4")) {
 		System.out.println("Cloruro de Plomo (IV)");
